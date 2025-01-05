41e49a09d37b83f17446502c105e292f7e7b120b
Mohamed Shebl <hamdysheblm@gmail.com>
2025-01-05 23:34:09 +0200

    Add: current user unit tests

---
+++ server/src/routes/APIs/__test__/current-user.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import { app } from '../../../app';
+
+it('responds with details about current user', async () => {
+	const signinData = await signin();
+
+	const response = await request(app)
+		.get('/api/users/currentUser')
+		.set('Cookie', signinData.cookie)
+		.send({})
+		.expect(200);
+
+	expect(response.body.currentUser.email).toEqual(signinData.user.email);
+});
+
+it('responds with null if not authenticated user', async () => {
+	const response = await request(app)
+		.get('/api/users/currentUser')
+		.send({})
+		.expect(200);
+
+	expect(response.body.currentUser).toBeNull();
+});
