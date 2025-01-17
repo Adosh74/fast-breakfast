264a762172f2dae1750a1d4303c2188a574205e8
Mohamed Shebl <hamdysheblm@gmail.com>
2025-01-17 22:31:23 +0200

    Add: item schema

---
+++ server/src/models/item.ts
@@ -0,0 +1,53 @@
+import { Schema, model, Model, Document } from 'mongoose';
+
+interface ItemAttrs {
+	name: string;
+	price: number;
+	avatar?: string;
+}
+
+interface ItemModel extends Model<ItemDoc> {
+	build(attrs: ItemAttrs): ItemDoc;
+}
+
+interface ItemDoc extends Document {
+	name: string;
+	price: number;
+	avatar: string;
+}
+
+const itemSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		avatar: {
+			type: String,
+			required: true,
+			default:
+				'https://i.pinimg.com/736x/53/3d/f3/533df3bc8e2a52135b632fa46316d3e7--middle-eastern-recipes-restaurant-recipes.jpg',
+		},
+	},
+	{
+		toJSON: {
+			transform: function (doc, ret) {
+				ret.id = ret._id;
+				delete ret._id;
+				delete ret.__v;
+			},
+		},
+	}
+);
+
+itemSchema.statics.build = (attrs: ItemAttrs) => {
+	return new Item(attrs);
+};
+
+const Item = model<ItemDoc, ItemModel>('Item', itemSchema);
+
+export { Item };
