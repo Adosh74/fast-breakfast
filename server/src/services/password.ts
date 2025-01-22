1db2dd89b12180108bedeba7f26a3d7020cd2469
Mohamed Shebl <hamdysheblm@gmail.com>
2025-01-22 22:53:27 +0200

    Add password pepper

--- server/src/services/password.ts
+++ server/src/services/password.ts
@@ -1,20 +1,25 @@
 import { scrypt, randomBytes } from 'crypto';
-import { promisify } from 'util'
+import { promisify } from 'util';
+import { serverEnv } from '../config';
 
-const scryptAsync = promisify(scrypt)
+const scryptAsync = promisify(scrypt);
 
 export class Password {
-    static async toHash(password: string) {
-        const salt = randomBytes(8).toString('hex');
-        const buf = (await scryptAsync(password, salt, 64)) as Buffer
+	static async toHash(password: string) {
+		const salt = randomBytes(8).toString('hex');
+		const buf = (await scryptAsync(password + serverEnv.pepper, salt, 64)) as Buffer;
 
-        return `${buf.toString('hex')}.${salt}`
-    }
+		return `${buf.toString('hex')}.${salt}`;
+	}
 
-    static async compare(storedPassword: string, suppliedPassword: string) {
-        const [hashedPassword, salt] = storedPassword.split('.');
-        const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+	static async compare(storedPassword: string, suppliedPassword: string) {
+		const [hashedPassword, salt] = storedPassword.split('.');
+		const buf = (await scryptAsync(
+			suppliedPassword + serverEnv.pepper,
+			salt,
+			64
+		)) as Buffer;
 
-        return hashedPassword === buf.toString('hex');
-    }
+		return hashedPassword === buf.toString('hex');
+	}
 }
