613330f071a424ee6a578a05a19c377235773c4a
Mohamed Shebl <hamdysheblm@gmail.com>
2025-01-24 22:26:00 +0200

    Add: get order handler and route

--- server/src/routes/APIs/order/index.ts
+++ server/src/routes/APIs/order/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { getOrderRouter } from './get-order';
 import { addOrderRouter } from './add-order';
+import { getDayOrdersRouter } from './get-day-orders';
 
 const router = Router();
 
 router.use(getOrderRouter);
 router.use(addOrderRouter);
+router.use(getDayOrdersRouter);
 
 export { router as orderRouter };
