7bcda416f63f433e1ae806c8eef2d21c8bdc37aa
Mohamed Shebl <hamdysheblm@gmail.com>
2025-01-25 19:47:08 +0200

    Add: log unhanddle error

--- server/src/middlewares/error-handler.ts
+++ server/src/middlewares/error-handler.ts
@@ -1,21 +1,25 @@
-import { NextFunction, Request, Response } from "express";
-import { CustomError } from "../errors/custom-error";
+import { NextFunction, Request, Response } from 'express';
+import { CustomError } from '../errors/custom-error';
+import { LOGGER } from '../logging';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-
-    if (err instanceof CustomError) {
-        res.status(err.statusCode).send({
-            errors: err.serializeErrors()
-        })
-    }
-    else {
-        res.status(400).send({
-            errors: [
-                {
-                    message: 'Some thing went wrong'
-                }
-            ]
-        })
-    }
-
-}
+export const errorHandler = (
+	err: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	if (err instanceof CustomError) {
+		res.status(err.statusCode).send({
+			errors: err.serializeErrors(),
+		});
+	} else {
+		LOGGER.error(err);
+		res.status(400).send({
+			errors: [
+				{
+					message: 'Some thing went wrong',
+				},
+			],
+		});
+	}
+};
