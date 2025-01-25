de8c02c4535c3cb8bd9a10de86b95a1384bda19e
Mohamed Shebl <hamdysheblm@gmail.com>
2025-01-25 23:35:33 +0200

    Add: orders route into main routes

--- server/src/routes/APIs/order/index.ts
+++ server/src/routes/APIs/order/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { getOrderRouter } from './get-order';
 import { addOrderRouter } from './add-order';
-import { getDayOrdersRouter } from './get-day-orders';
+import { getMyOrdersRouter } from './my-orders';
+import { getDayOrdersRouter } from './day-orders';
 
 const router = Router();
 
 router.use(getOrderRouter);
 router.use(addOrderRouter);
+router.use(getMyOrdersRouter);
 router.use(getDayOrdersRouter);
 
 export { router as orderRouter };
