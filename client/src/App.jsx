902fd731ace29a12ec454f92aa5afaf981c77f4a
Mohamed Shebl <hamdysheblm@gmail.com>
2025-01-25 23:40:31 +0200

    Add: routes for new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
+import AdminOrders from './pages/AdminOrders';
+import Receipts from './pages/Receipts';
 import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
@@ -10,9 +12,13 @@ export default function App() {
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        
         <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Home />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/admin/orders" element={<AdminOrders />} />
+          <Route path="/admin/receipts" element={<Receipts />} />
         </Route>
+        
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
