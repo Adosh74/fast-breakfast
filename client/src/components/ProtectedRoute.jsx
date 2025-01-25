606cc9f3677fcbf5304d892869e0f8bab80358ff
Mohamed Shebl <hamdysheblm@gmail.com>
2025-01-25 23:39:00 +0200

    Add: protect route componant

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -4,6 +4,7 @@ import axiosInstance from '../api/axios';
 
 export default function ProtectedRoute() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userRole, setUserRole] = useState('');
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -12,8 +13,7 @@ export default function ProtectedRoute() {
         const response = await axiosInstance.get('/api/users/currentuser');
         if (response.data.currentUser) {
           setIsAuthenticated(true);
-        } else {
-          setIsAuthenticated(false);
+          setUserRole(response.data.currentUser.role);
         }
       } catch (err) {
         console.error('Error checking authentication:', err);
@@ -26,9 +26,14 @@ export default function ProtectedRoute() {
     checkAuth();
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
+  if (loading) return <div>Loading...</div>;
+
+  if (!isAuthenticated) return <Navigate to="/signin" />;
+
+  // Add admin route protection
+  if (window.location.pathname.startsWith('/admin') && userRole !== 'admin') {
+    return <Navigate to="/" />;
   }
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/signin" />;
+  return <Outlet />;
 }
