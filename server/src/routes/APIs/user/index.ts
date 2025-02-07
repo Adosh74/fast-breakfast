0fa4f35061a2d024ed8e8f1638350027e2f194e6
Mohamed Shebl <hamdysheblm@gmail.com>
2025-02-08 00:04:55 +0200

    Add: all user router and update user router into main user routes

--- server/src/routes/APIs/user/index.ts
+++ server/src/routes/APIs/user/index.ts
@@ -4,6 +4,8 @@ import { signinRouter } from './signin';
 import { currentUserRouter } from './current-user';
 import { signoutRouter } from './signout';
 import { signupRouter } from './signup';
+import { allUsersRouter } from './all-users';
+import { updateUserRouter } from './update-user';
 
 const userRoutes = Router();
 
@@ -11,5 +13,7 @@ userRoutes.use(currentUserRouter);
 userRoutes.use(signinRouter);
 userRoutes.use(signoutRouter);
 userRoutes.use(signupRouter);
+userRoutes.use(allUsersRouter);
+userRoutes.use(updateUserRouter);
 
 export { userRoutes };
