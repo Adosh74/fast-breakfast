3b4a9d1039559de6663811eb08ad323827093235
Mohamed Shebl <hamdysheblm@gmail.com>
2025-02-08 00:07:42 +0200

    Add: statistics and user routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import AdminOrders from './pages/AdminOrders';
 import Receipts from './pages/Receipts';
 import ProtectedRoute from './components/ProtectedRoute';
+import Users from './pages/Users';
+import Statistics from './pages/Statistics';
 
 export default function App() {
   return (
@@ -17,6 +19,8 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/admin/orders" element={<AdminOrders />} />
           <Route path="/admin/receipts" element={<Receipts />} />
+          <Route path="/admin/users" element={<Users />} />
+          <Route path="/statistics" element={<Statistics />} />
         </Route>
         
         <Route path="*" element={<Navigate to="/" />} />
