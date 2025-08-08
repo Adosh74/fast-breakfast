e2f5e141de36a8dad6818afa924965331f66f17d
Mohamed Shebl <hamdysheblm@gmail.com>
2025-08-08 15:32:07 +0300

    feat(routing): add header and manage-items page to protected routes
    - Include Header component in all protected route pages for consistent UI
    - Add new ManageItems page to the protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Receipts from './pages/Receipts';
 import ProtectedRoute from './components/ProtectedRoute';
 import Users from './pages/Users';
 import Statistics from './pages/Statistics';
+import Header from './components/Header';
+import ManageItems from './pages/ManageItems';
 
 export default function App() {
   return (
@@ -16,11 +18,12 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         
         <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin/orders" element={<AdminOrders />} />
-          <Route path="/admin/receipts" element={<Receipts />} />
-          <Route path="/admin/users" element={<Users />} />
-          <Route path="/statistics" element={<Statistics />} />
+          <Route path="/" element={<><Header /><Home /></>} />
+          <Route path="/admin/orders" element={<><Header /><AdminOrders /></>} />
+          <Route path="/admin/receipts" element={<><Header /><Receipts /></>} />
+          <Route path="/admin/users" element={<><Header /><Users /></>} />
+          <Route path="/statistics" element={<><Header /><Statistics /></>} />
+          <Route path="/manage-items" element={<><Header /><ManageItems /></>} />
         </Route>
         
         <Route path="*" element={<Navigate to="/" />} />
