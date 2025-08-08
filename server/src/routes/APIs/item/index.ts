7a4271e014f6539cf951833a585bb830a3b4d0db
Mohamed Shebl <hamdysheblm@gmail.com>
2025-08-08 15:30:34 +0300

    feat(item): add update item route to item routes
    Add the update item router import and usage to the item routes file to enable item updates functionality

--- server/src/routes/APIs/item/index.ts
+++ server/src/routes/APIs/item/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { itemsListRouter } from './items-list';
 import { addItemRouter } from './add-item';
+import { updateItemRouter } from './update-item';
 
 const router = Router();
 
 router.use(addItemRouter);
 router.use(itemsListRouter);
+router.use(updateItemRouter);
 
 export { router as itemRoutes };
